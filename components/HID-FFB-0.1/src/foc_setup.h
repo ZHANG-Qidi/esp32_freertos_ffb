f359ebad77f678a7ee7feaffc51389e8eda937c4
ZHANG-Qidi <grunt4991@gmail.com>
2026-09-13 14:30:14 +0900

    将组件设置移回组件

--- components/HID-FFB-0.1/src/foc_setup.h
+++ components/HID-FFB-0.1/src/foc_setup.h
@@ -9,6 +9,10 @@ extern "C" {
 #define VOLTAGE_LIMIT (6.0f)
 #define VOLTAGE_SENSOR_ALIGN (1.0f)
 #define COMMANDER_BAUD_RATE (115200)
+#define MOTOR_U (CONFIG_FOC_MOTOR_U)
+#define MOTOR_V (CONFIG_FOC_MOTOR_V)
+#define MOTOR_W (CONFIG_FOC_MOTOR_W)
+#define MOTOR_EN (CONFIG_FOC_MOTOR_EN)
 extern BLDCMotor motor;
 extern MagneticSensorSPI sensor;
 extern void foc_setup(void);
